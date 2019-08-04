d592368f9f008ee7893eb04633fe837cd055dd92
김병철 <nomakan@naver.com>
2019-08-04 22:58:12 +0900

    Sensor명 변경

--- Bnb_DeviceInfo_Test/Bnb_DHT11_Sensor.cpp
+++ Bnb_DeviceInfo_Test/Bnb_DHT11_Sensor.cpp
@@ -7,10 +7,12 @@
 
 void Bnb_DHT11_Sensor::_publish()
 {
+	_getJSON().addTag("sensor", "Humidity");
 	_getJSON().addValue("value", readHumidity());
 	Serial.println(toString());
 	BNBPubSubClient->publish(toString());
 
+	_getJSON().addTag("sensor", "Temperature");
 	_getJSON().addValue("value", readTemperature());
 	Serial.println(toString());
 	BNBPubSubClient->publish(toString());
